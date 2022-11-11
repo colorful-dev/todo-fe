4bceba13018ebf8c4ceb307ab8d394af9489b1bc
alexzhang1030 <1642114555@qq.com>
2022-11-11 21:07:22 +0800

    feat: init item

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div
+    w-full h-40px rounded-6px
+    bg-blue-500 flex items-center pl-20px box-border
+  >
+    <Checkbox />
+    Todo fe dev
+  </div>
+</template>
+
+<style scoped></style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
+const listLength = 10
 </script>
 
 <template>
-  <div mt-10vh>
+  <div mt-10vh max-w="40%">
     <h1 text-60px text="#3C6AFF" font-bold mb-1>
       Todo List
     </h1>
     <TodoInput />
+
+    <div flex="~ wrap col" gap-10px first-of-type:mt-10px>
+      <TodoItem v-for="i in listLength" :key="i" />
+    </div>
   </div>
 </template>
 
